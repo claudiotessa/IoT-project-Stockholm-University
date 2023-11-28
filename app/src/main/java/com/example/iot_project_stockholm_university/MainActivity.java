52e50378da0b007502e301fd528e1d79cc90cbb7
Claudio Tessa <tessa.claudio04@gmail.com>
2023-11-28 14:50:11 +0100

    first commit
    project created

---
+++ app/src/main/java/com/example/iot_project_stockholm_university/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.iot_project_stockholm_university;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
