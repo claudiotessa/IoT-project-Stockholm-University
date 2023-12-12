b6180c697529d81ca4e8ec59fbe68bac1faabe30
AleValle <48917329+alexsockz@users.noreply.github.com>
2023-12-12 18:07:50 +0100

    wattage.py now accepts requests to turn on and off any connected actuators

--- raspberryFiles/wattage.py
+++ raspberryFiles/wattage.py
@@ -8,12 +8,13 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
+import subprocess
 
 # Set MQTT broker and topic
 broker = "test.mosquitto.org"	# Broker 
 
-pub_topic = "iotProject/sensors"    # send messages to this topic
-
+pub_topic = "iotProject/sensors" #send data of sensor
+sub_topic = ["iotProject/devices", "iotProject/file"] #control for the change in the actuator status
 ############### Sensor section ##################
 def get_wattage():
 
@@ -21,7 +22,7 @@ def get_wattage():
     myobj = {'pi': "broker"}
 
     x = requests.post(url, json = myobj)
-    print(x.text)
+    #print(x.text)
     y = json.loads(x.text)
     f = open('detections.csv', 'a')
     writer = csv.writer(f, lineterminator = '\n')
@@ -49,6 +50,24 @@ def on_disconnect(client, userdata, rc):
 	
 def on_log(client, userdata, level, buf):		# Message is in buf
     print("MQTT Log: " + str(buf))
+
+#when getting message from a subscribed topic 
+# {"cmd":"switch", "id": id number, "onoff": on\off status}
+# {"cmd":"getloglog", "id": id number}
+
+def on_message(client, userdata, message): 
+    data = str(message.payload.decode("utf-8"))
+    print("message received " ,)
+    print("message topic=",message.topic)
+    print("message qos=",message.qos)
+    print("message retain flag=",message.retain)
+    try:
+        dictCommand = json.loads(data)
+        if dictCommand["cmd"] == "switch" :
+            subprocess.run(["tdtool", "--"+dictCommand["onoff"], dictCommand["id"]])
+        #elif dictCommand["cmd"] == "getlog" :
+    except ValueError as error:
+        return error
 	
 # Connect functions for MQTT
 client = mqtt.Client()
@@ -56,12 +75,14 @@ client.on_connect = on_connect
 client.on_disconnect = on_disconnect
 client.on_publish = on_publish
 client.on_log = on_log
+client.on_message= on_message
 
 # Connect to MQTT 
 print("Attempting to connect to broker " + broker)
 client.connect(broker)	# Broker address, port and keepalive (maximum period in seconds allowed between communications with the broker)
+for i in sub_topic :
+    client.subscribe(i)
 client.loop_start()
-
 
 # Loop that publishes message
 while True:
